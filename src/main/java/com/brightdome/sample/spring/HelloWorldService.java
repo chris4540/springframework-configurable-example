682da773eb1a2174eee0ab09126e4d18107281c4
Chris C.H. Lin <chris4540@gmail.com>
2023-08-15 01:09:33 +0100

    Add spring framework code

---
+++ src/main/java/com/brightdome/sample/spring/HelloWorldService.java
@@ -0,0 +1,10 @@
+package com.brightdome.sample.spring;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+  public void sayHello() {
+    System.out.println("Hello world!");
+  }
+}
